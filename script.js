dc4635e227ecc05fe613ef86e05daebf10c91a5c
omonigho91 <62224958+omonigho91@users.noreply.github.com>
2020-03-27 23:20:28 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+var name = "Sophia Otoide"
+
+document.write(name + '<br/>' + '<br/>');
+
+var courses = ['html',  '  css', '  javaScript.'];
+document.write(courses + '<br/>' + '<br/>');
+
+
+var oddNumber = [];
+
+for (i = 1; i <= 200; i += 2 )
+
+{
+
+oddNumber.push(i) 
+
+}
+
+
+
+
+document.write(oddNumber)
+
+alert(oddNumber)
+
+
+
+
